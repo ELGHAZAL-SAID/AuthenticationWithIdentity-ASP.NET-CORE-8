00f218a19c03dfc6966b15c61a2e2c1c417d9377
Said El ghazal <selghazal@unibitsoft.com>
2025-01-10 15:00:39 +0100

    Add project files.

---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace DOTNET_AUTHENTICATION.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
